5ae1b580cbef9f6b9efcae75291e9454de711a78
Rory Connolly <Rory.Connolly@liberty-it.co.uk>
2017-10-02 22:16:34 +0100

    Adding linting
    Adding Linting

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,17 +2,24 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const element = (
+  <h1>
+    Hello,
+  </h1>
+);
 class App extends Component {
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome two React</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <p> Just some extra text </p>
       </div>
     );
   }
